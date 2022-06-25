726989891dd18b2e664fbc4b29771d90548f518d
Ashur Kanwal <ashurkanwal@yahoo.com>
2022-06-25 09:28:48 +0100

    created structure

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -3,7 +3,27 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='Rtmplt__header'>
+        <div className='Rtmplt__header-row'>
+          This is the Flex row and I want it at the top of my flex.
+          This is the Flex row and I want it at the top of my flex.
+          This is the Flex row and I want it at the top of my flex
+        </div>
+        <div className='Rtmplt__header-cols'>
+          <div className='Rtmplt__header-col-1'>Four</div>
+          <div className='Rtmplt__header-col-2'>
+            <p>React component here</p>
+            <p>React component here</p>
+            <p>React component here</p>
+            <p>React component here</p>
+            <p>React component here</p>
+            <p>React component here</p>
+            <p>React component here</p>
+          </div>
+        </div>
+        
+      
+    </div>
   )
 }
 
