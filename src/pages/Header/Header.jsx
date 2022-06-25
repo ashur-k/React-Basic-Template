e279dcd788a8da3ffc733d6f1946c85ce5e5666a
Ashur Kanwal <ashurkanwal@yahoo.com>
2022-06-25 09:54:07 +0100

    created react array

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import './header.css';
 
 const Header = () => {
+
+  const component_values = [
+    "React component 1 here",
+    "React component 2 here",
+    "React component 3 here",
+    "React component 4 here",
+    "React component 5 here",
+    "React component 6 here",
+    "React component 7 here"
+  ]
+
   return (
     <div className='Rtmplt__header'>
         <div className='Rtmplt__header-row'>
@@ -12,17 +23,13 @@ const Header = () => {
         <div className='Rtmplt__header-cols'>
           <div className='Rtmplt__header-col-1'>Four</div>
           <div className='Rtmplt__header-col-2'>
-            <p>React component here</p>
-            <p>React component here</p>
-            <p>React component here</p>
-            <p>React component here</p>
-            <p>React component here</p>
-            <p>React component here</p>
-            <p>React component here</p>
+            {
+              component_values.map(value => (
+                <p>{value}</p>
+              ))
+            }
           </div>
         </div>
-        
-      
     </div>
   )
 }
